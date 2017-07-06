9f8bcc6eb377836f53bb91f3edd586058db2442f
Vaibhav Khulbe <kvaibhav01@live.in>
2017-07-06 23:16:52 +0530

    Created MainActivity.java file

---
+++ app/src/main/java/info/introapp/MainActivity.java
@@ -0,0 +1,44 @@
+package info.androidhive.introslider;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.text.Html;
+import android.text.method.LinkMovementMethod;
+import android.view.View;
+import android.widget.TextView;
+
+import com.theIntro.android.App.R;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        findViewById(R.id.btn_play_again).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                // We normally won't show the welcome slider again in real app
+                // but this is for testing
+                PrefManager prefManager = new PrefManager(getApplicationContext());
+
+                // make first time launch TRUE
+                prefManager.setFirstTimeLaunch(true);
+
+                startActivity(new Intent(MainActivity.this, WelcomeActivity.class));
+                finish();
+            }
+        });
+
+        TextView textView =(TextView)findViewById(R.id.textView);
+        textView.setClickable(true);
+        textView.setMovementMethod(LinkMovementMethod.getInstance());
+        String text = "<a href='https://github.com/Kvaibhav01/IntroApp'> Star or Fork this project on GitHub </a>";
+        textView.setText(Html.fromHtml(text));
+    }
+}
